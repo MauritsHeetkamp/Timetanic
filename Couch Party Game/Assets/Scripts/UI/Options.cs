cb6941034f63ae715e788325cbc705523a34ddfb
Maurits Heetkamp <maurits@vrowl.nl>
2021-03-03 14:29:56 +0100

    Added base for dynamic UI movement
    UI System for both controller, keypad and mouse

---
+++ Couch Party Game/Assets/Scripts/UI/Options.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Options : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer; // Handles the audio globally
+    [SerializeField] string masterAudioString = "Master";
+    [SerializeField] string sfxAudioString = "SFX";
+    [SerializeField] string backgroundAudioString = "Background";
+
+    // Sets the quality level
+    public void SetQualityLevel(int qualityLevelIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityLevelIndex, true);
+    }
+
+    // Sets the master volume
+    public void SetMasterVolume(Slider volumeSlider)
+    {
+        audioMixer.SetFloat(masterAudioString, volumeSlider.value);
+    }
+
+    // Sets the sfx volume
+    public void SetSFXVolume(Slider volumeSlider)
+    {
+        audioMixer.SetFloat(sfxAudioString, volumeSlider.value);
+    }
+
+    // Sets the background volume
+    public void SetBackgroundVolume(Slider volumeSlider)
+    {
+        audioMixer.SetFloat(backgroundAudioString, volumeSlider.value);
+    }
+}
